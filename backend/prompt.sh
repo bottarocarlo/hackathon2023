61fcd26c4c0ad8ae0f00e707680176c4e2b58a09
Nicola Gutierrez <nicolagutierrez.ng@gmail.com>
2024-01-09 18:16:00 +0100

    feat: temp final version

--- backend/prompt.sh
+++ backend/prompt.sh
@@ -2,4 +2,4 @@
 rm test.txt
 rm final.txt
 ollama run llama2 "Get all information about techinical System Requirements from this text: $(cat extracted_text.txt)" >> test.txt
-ollama run llama2 "Considering the requirements: $(cat test.txt). Substitute the values (not the key) following JSON structure: $(cat structure.json) and return just the json, not other text." >> final.txt
+ollama run llama2 "Considering the requirements: $(cat test.txt). Create a JSON following rules: $(cat template.md) and return just the json with this structure (not other text): $(cat structure.json)" >> final.txt
